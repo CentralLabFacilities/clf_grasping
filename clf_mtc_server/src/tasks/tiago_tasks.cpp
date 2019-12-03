c283afb2f981e20cd7f7a7f3b45e47d344b0bf6a
Markus Vieth <mvieth@techfak.uni-bielefeld.de>
2019-12-03 17:25:41 +0100

    Switch to new grasp generator for tiago tasks

--- clf_mtc_server/src/tasks/tiago_tasks.cpp
+++ clf_mtc_server/src/tasks/tiago_tasks.cpp
@@ -1,5 +1,8 @@
 #include "clf_mtc_server/tasks/tiago_tasks.h"
 
+#include "clf_mtc_server/stages/generate_all_grasp_pose.h"
+#include "clf_mtc_server/stages/grasp_generator_2_0.h"
+
 #include <ros/ros.h>
 
 #include <moveit/task_constructor/task.h>
@@ -42,8 +45,9 @@ Task TiagoTasks::createPickTask(std::string id)
   t.add(std::move(connect));
 
   // grasp generator
-  auto grasp_generator = std::make_unique<stages::GenerateGraspPose>("generate grasp pose");
-  grasp_generator->setAngleDelta(.2);
+  //auto grasp_generator = std::make_unique<stages::GenerateGraspPose>("generate grasp pose");
+  auto grasp_generator = std::make_unique<stages::GraspGenerator>("grasp generator");
+  //grasp_generator->setAngleDelta(.2);
   grasp_generator->setPreGraspPose("open");
   grasp_generator->setGraspPose("closed");
   grasp_generator->setMonitoredStage(initial_stage);
