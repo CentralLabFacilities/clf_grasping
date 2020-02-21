69148d577f5e8e75b213abb3f71d58526a81dac8
Markus Vieth <mvieth@techfak.uni-bielefeld.de>
2020-02-21 12:30:57 +0100

    Fix warning

--- clf_mtc_server/include/clf_mtc_server/server.h
+++ clf_mtc_server/include/clf_mtc_server/server.h
@@ -42,13 +42,13 @@ private:
   clf_grasping_msgs::PickFeedback pickFeedback_;
   clf_grasping_msgs::PickResult pickResult_;
 
+  actionlib::SimpleActionServer<clf_grasping_msgs::PlanPickAction> planPickAs_;
+  void executePlanPick(const clf_grasping_msgs::PlanPickGoalConstPtr& goal);
+
   actionlib::SimpleActionServer<clf_grasping_msgs::PlaceAction> placeAs_;
   void executePlace(const clf_grasping_msgs::PlaceGoalConstPtr& goal);
   clf_grasping_msgs::PlaceFeedback placeFeedback_;
   clf_grasping_msgs::PlaceResult placeResult_;
-
-  actionlib::SimpleActionServer<clf_grasping_msgs::PlanPickAction> planPickAs_;
-  void executePlanPick(const clf_grasping_msgs::PlanPickGoalConstPtr& goal);
 
   actionlib::SimpleActionServer<clf_grasping_msgs::PlanPlaceAction> planPlaceAs_;
   void executePlanPlace(const clf_grasping_msgs::PlanPlaceGoalConstPtr& goal);
