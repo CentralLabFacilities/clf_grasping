8229015a3c9feeaa4bbf0479495a80a1d9ada59d
Markus Vieth <mvieth@techfak.uni-bielefeld.de>
2020-03-02 12:49:50 +0100

    Add option to allow gripper support collision

--- clf_mtc_server/src/tasks/tiago_tasks.cpp
+++ clf_mtc_server/src/tasks/tiago_tasks.cpp
@@ -36,6 +36,9 @@ Task TiagoTasks::createPickTask(std::string id, std::string support_id)
   Task t("tiago_grasp");
   std::string eef = "gripper";
   std::string arm = "arm_torso";  // arm
+  // If gripper_fingers is not empty, they will be allowed to touch the support surface
+  //std::vector<std::string> gripper_fingers {"gripper_left_finger_link", "gripper_right_finger_link"};
+  std::vector<std::string> gripper_fingers;
 
   Stage* initial_stage = nullptr;
   auto initial = std::make_unique<stages::CurrentState>("current state");
@@ -68,10 +71,46 @@ Task TiagoTasks::createPickTask(std::string id, std::string support_id)
   grasp_generator->setPreGraspPose("open");
   grasp_generator->setGraspPose("closed");
   grasp_generator->setMonitoredStage(initial_stage);
+  std::unique_ptr<stages::SimpleGrasp> grasp;
+  if(!gripper_fingers.empty())
+  {
+    // forward properties from generator's to IK's solution (bottom -> up)
+    //grasp_generator->setForwardedProperties({ "object", "eef", "pregrasp", "grasp" });
+    // allow inheritance in top -> down fashion as well
+    grasp_generator->properties().configureInitFrom(Stage::PARENT, { "object", "eef" });
 
-  auto grasp = std::make_unique<stages::SimpleGrasp>(std::move(grasp_generator));
+    auto grasp_generator_wrapper = std::make_unique<SerialContainer>("grasp generator wrapper");
+    grasp_generator_wrapper->properties().configureInitFrom(Stage::PARENT | Stage::INTERFACE, {"object", "eef"});
+    grasp_generator_wrapper->properties().declare<std::string>("grasp");
+    grasp_generator_wrapper->properties().declare<std::string>("pregrasp");
+    grasp_generator_wrapper->properties().declare<std::string>("object");
+    grasp_generator_wrapper->properties().declare<std::string>("eef");
+    grasp_generator_wrapper->properties().declare<std::string>("target_pose");
+    //grasp_generator_wrapper->properties().configureInitFrom(Stage::INTERFACE, { "target_pose" });
+    grasp_generator_wrapper->insert(std::move(grasp_generator), 0);
+
+    {
+      auto allow_gripper_touch_support = new stages::ModifyPlanningScene("allow gripper support collision");
+      //allow_gripper_touch_support->properties().configureInitFrom(Stage::INTERFACE, { "target_pose" });
+      allow_gripper_touch_support->setForwardedProperties({"target_pose", "eef", "grasp", "pregrasp" });
+      allow_gripper_touch_support->setCallback([support_id, gripper_fingers](const planning_scene::PlanningScenePtr& scene, const PropertyMap& p) {
+        ROS_INFO("Allowing collision between support and gripper");
+        collision_detection::AllowedCollisionMatrix& acm = scene->getAllowedCollisionMatrixNonConst();
+        for(const std::string& finger : gripper_fingers)
+          acm.setEntry(finger, support_id, true);
+      });
+      grasp_generator_wrapper->insert(Stage::pointer(allow_gripper_touch_support), 0);
+    }
+
+    grasp = std::make_unique<stages::SimpleGrasp>(std::move(grasp_generator_wrapper));
+  }
+  else
+  {
+    grasp = std::make_unique<stages::SimpleGrasp>(std::move(grasp_generator));
+  }
   grasp->setIKFrame(Eigen::Isometry3d::Identity(), tool_frame_);
   grasp->setProperty("max_ik_solutions", 1u);
+  //grasp->setProperty("ignore_collisions", true);
 
   auto pick = std::make_unique<stages::Pick>(std::move(grasp));
   pick->setProperty("eef", eef);
@@ -119,6 +158,18 @@ Task TiagoTasks::createPickTask(std::string id, std::string support_id)
       acm.setEntry(object, support, false);
     });
     pick->insert(Stage::pointer(disallow_touch), -1); // insert at the end (after the last element)
+  }
+
+  if(!gripper_fingers.empty())
+  {
+    auto allow_gripper_touch_support = new stages::ModifyPlanningScene("allow gripper support collision");
+    allow_gripper_touch_support->setCallback([support_id, gripper_fingers](const planning_scene::PlanningScenePtr& scene, const PropertyMap& p) {
+      ROS_INFO("Allowing collision between support and gripper");
+      collision_detection::AllowedCollisionMatrix& acm = scene->getAllowedCollisionMatrixNonConst();
+      for(const std::string& finger : gripper_fingers)
+        acm.setEntry(finger, support_id, true);
+    });
+    pick->insert(Stage::pointer(allow_gripper_touch_support), 0);
   }
 
   t.add(std::move(pick));
